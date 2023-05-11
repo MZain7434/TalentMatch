ed5da5e9a4c094aca30ebc68b1ffa2a90e78a114
MZain7434 <zainm7434@gmail.com>
2023-05-11 12:52:32 +0500

    Signup Complete

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,36 +1,35 @@
 import React, { createContext, useState, useContext } from "react";
-import AnimationRevealPage from "../../helpers/AnimationRevealPage.js";
-import { Container as ContainerBase } from "../../Components/Misc/Layouts.js";
-import apiList from "../../Components/lib/apiList.js";
+import { useNavigate, Link } from "react-router-dom";
 import tw from "twin.macro";
+import axios from "axios";
 import styled from "styled-components";
 import { css } from "styled-components/macro"; //eslint-disable-line
+import { ReactComponent as SignUpIcon } from "feather-icons/dist/icons/user-plus.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import apiList from "../../Components/lib/apiList.js";
+import { Container as ContainerBase } from "../../Components/Misc/Layouts.js";
+import AnimationRevealPage from "../../helpers/AnimationRevealPage.js";
 import illustration from "../../images/signup-illustration.svg";
 import logo from "../../images/logo.svg";
-import { ReactComponent as SignUpIcon } from "feather-icons/dist/icons/user-plus.svg";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import { SetPopupContext } from "../../App.js";
-import Login from "../login/Login.js";
-
-
 
 const Container = tw(
   ContainerBase
-  )`min-h-screen bg-primary-900 text-white font-medium flex justify-center -m-8`;
-  const Content = tw.div`max-w-screen-xl m-0 sm:mx-20 sm:my-16 bg-white text-gray-900 shadow sm:rounded-lg flex justify-center flex-1`;
-  const MainContainer = tw.div`lg:w-1/2 xl:w-5/12 p-6 sm:p-12`;
-  const LogoLink = tw.a``;
-  const LogoImage = tw.img`h-12 mx-auto`;
-  const MainContent = tw.div`mt-10 flex flex-col items-center`;
-  const Heading = tw.h1`text-2xl xl:text-3xl font-extrabold`;
-  const FormContainer = tw.div`w-full flex-1 mt-8`;
-  
-  const Form = tw.form`mx-auto max-w-xs`;
-  const Dropdown = tw.select`w-full font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-gray-600  py-2  px-3  rounded-lg  shadow-sm  focus:outline-none  focus:border-gray-400 mt-5 first:mt-0 appearance-none`;
-  
-  const Input = tw.input`w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 first:mt-0`;
-  const SubmitButton = styled.button`
+)`min-h-screen bg-primary-900 text-white font-medium flex justify-center -m-8`;
+const Content = tw.div`max-w-screen-xl m-0 sm:mx-20 sm:my-16 bg-white text-gray-900 shadow sm:rounded-lg flex justify-center flex-1`;
+const MainContainer = tw.div`lg:w-1/2 xl:w-5/12 p-6 sm:p-12`;
+const LogoLink = tw.a``;
+const LogoImage = tw.img`h-12 mx-auto`;
+const MainContent = tw.div`mt-10 flex flex-col items-center`;
+const Heading = tw.h1`text-2xl xl:text-3xl font-extrabold`;
+const FormContainer = tw.div`w-full flex-1 mt-8`;
+
+const Form = tw.form`mx-auto max-w-xs`;
+const Dropdown = tw.select`w-full font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-gray-600  py-2  px-3  rounded-lg  shadow-sm  focus:outline-none  focus:border-gray-400 mt-5 first:mt-0 appearance-none`;
+
+const Input = tw.input`w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 first:mt-0`;
+const SubmitButton = styled.button`
   ${tw`mt-5 tracking-wide font-semibold bg-primary-500 text-gray-100 w-full py-4 rounded-lg hover:bg-primary-900 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none`}
   .icon {
     ${tw`w-6 h-6 -ml-2`}
@@ -38,32 +37,32 @@ const Container = tw(
   .text {
     ${tw`ml-3`}
   }
-  `;
-  const IllustrationContainer = tw.div`sm:rounded-r-lg flex-1 bg-purple-100 text-center hidden lg:flex justify-center`;
-  const IllustrationImage = styled.div`
+`;
+const IllustrationContainer = tw.div`sm:rounded-r-lg flex-1 bg-purple-100 text-center hidden lg:flex justify-center`;
+const IllustrationImage = styled.div`
   ${(props) => `background-image: url("${props.imageSrc}");`}
   ${tw`m-12 xl:m-16 w-full max-w-lg bg-contain bg-center bg-no-repeat`}
-  `;
-  
-  export default ({
-    logoLinkUrl = "/",
-    illustrationImageSrc = illustration,
-    headingText = "Sign Up For TalentMatch",
-    submitButtonText = "Sign Up",
-    SubmitButtonIcon = SignUpIcon,
-    tosUrl = "/tos",
-    privacyPolicyUrl = "/privacypolicy",
-    signInUrl = "/login",
-    onSubmit,
-  }) => {
-    const [name, setName] = useState("");
+`;
+
+export default ({
+  logoLinkUrl = "/",
+  illustrationImageSrc = illustration,
+  headingText = "Sign Up For TalentMatch",
+  submitButtonText = "Sign Up",
+  SubmitButtonIcon = SignUpIcon,
+  tosUrl = "/tos",
+  privacyPolicyUrl = "/privacypolicy",
+  signInUrl = "/login",
+}) => {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [accountType, setAccountType] = useState("applicant");
+  const [accountType, setAccountType] = useState("candidate");
   const handleFormSubmit = (e) => {
     e.preventDefault();
     if (!name || !email || !password || !accountType) {
-      alert("Please fill out all the feilds");
+      toast.error("Please fill out all the feilds");
       return;
     }
     const payload = {
@@ -81,96 +80,102 @@ const Container = tw(
     axios
       .post(apiList.signup, payload, config)
       .then((response) => {
-        localStorage.setItem("token", response.data.token);
-        localStorage.setItem("type", response.data.type);
-        
-        // if (response.data.token) {
-        //   history.push('/login');
-        //   return <Login />;
-        // }
-        console.log(response.data);
+        if (response.data.token) {
+          localStorage.setItem("TalentMatch_token", response.data.token);
+          localStorage.setItem("TalentMatch_type", response.data.type);
+          localStorage.setItem("TalentMatch_name", response.data.name);
+          if (response.data.type == "candidate") {
+            navigate("/candidate");
+          } else {
+            navigate("/recruiter");
+          }
+        }
       })
       .catch((err) => {
-        console.log(err.response);
+        const message = err.response.data.message;
+        toast.error(message);
       });
   };
   return (
-    <AnimationRevealPage>
-      <Container>
-        <Content>
-          <MainContainer>
-            <LogoLink>
-              <Link to={logoLinkUrl}>
-                <LogoImage src={logo} />
-              </Link>
-            </LogoLink>
-            <MainContent>
-              <Heading>{headingText}</Heading>
-              <FormContainer>
-                <Form onSubmit={handleFormSubmit}>
-                  <Input
-                    type="name"
-                    placeholder="Name"
-                    onChange={(e) => setName(e.target.value)}
-                    value={name}
-                  />
-                  <Input
-                    type="email"
-                    placeholder="Email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                  />
-                  <Input
-                    type="password"
-                    placeholder="Password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                  />
-                  <Dropdown
-                    name="accountType"
-                    id="accountTypeDropdown"
-                    onChange={(e) => setAccountType(e.target.value)}
-                  >
-                    <option value="applicant">Candidate</option>
-                    <option value="recruiter">Recruiter</option>
-                  </Dropdown>
-                  <SubmitButton>
-                    <Link to="/login" />
-                    <SubmitButtonIcon className="icon" />
-                    <span className="text">{submitButtonText}</span>
-                  </SubmitButton>
-                  <p tw="mt-6 text-xs text-gray-600 text-center">
-                    I agree to abide by TalentMatch's{" "}
-                    <Link to={tosUrl}>
-                      <span tw="border-b border-gray-500 border-dotted">
-                        Terms of Service
-                      </span>
-                    </Link>{" "}
-                    and its{" "}
-                    <Link to={privacyPolicyUrl}>
-                      <span tw="border-b border-gray-500 border-dotted">
-                        Privacy Policy
-                      </span>
-                    </Link>
-                  </p>
+    <>
+      <ToastContainer />
+      <AnimationRevealPage>
+        <Container>
+          <Content>
+            <MainContainer>
+              <LogoLink>
+                <Link to={logoLinkUrl}>
+                  <LogoImage src={logo} />
+                </Link>
+              </LogoLink>
+              <MainContent>
+                <Heading>{headingText}</Heading>
+                <FormContainer>
+                  <Form onSubmit={handleFormSubmit}>
+                    <Input
+                      type="name"
+                      placeholder="Name"
+                      onChange={(e) => setName(e.target.value)}
+                      value={name}
+                    />
+                    <Input
+                      type="email"
+                      placeholder="Email"
+                      onChange={(e) => setEmail(e.target.value)}
+                      value={email}
+                    />
+                    <Input
+                      type="password"
+                      placeholder="Password"
+                      onChange={(e) => setPassword(e.target.value)}
+                      value={password}
+                    />
+                    <Dropdown
+                      name="accountType"
+                      id="accountTypeDropdown"
+                      onChange={(e) => setAccountType(e.target.value)}
+                    >
+                      <option value="candidate">Candidate</option>
+                      <option value="recruiter">Recruiter</option>
+                    </Dropdown>
+                    <SubmitButton>
+                      <Link to="/login" />
+                      <SubmitButtonIcon className="icon" />
+                      <span className="text">{submitButtonText}</span>
+                    </SubmitButton>
+                    <p tw="mt-6 text-xs text-gray-600 text-center">
+                      I agree to abide by TalentMatch's{" "}
+                      <Link to={tosUrl}>
+                        <span tw="border-b border-gray-500 border-dotted">
+                          Terms of Service
+                        </span>
+                      </Link>{" "}
+                      and its{" "}
+                      <Link to={privacyPolicyUrl}>
+                        <span tw="border-b border-gray-500 border-dotted">
+                          Privacy Policy
+                        </span>
+                      </Link>
+                    </p>
 
-                  <p tw="mt-8 text-sm text-gray-600 text-center">
-                    Already have an account?{" "}
-                    <Link to={signInUrl}>
-                      <span tw="border-b border-gray-500 border-dotted">
-                        Sign In
-                      </span>
-                    </Link>
-                  </p>
-                </Form>
-              </FormContainer>
-            </MainContent>
-          </MainContainer>
-          <IllustrationContainer>
-            <IllustrationImage imageSrc={illustrationImageSrc} />
-          </IllustrationContainer>
-        </Content>
-      </Container>
-    </AnimationRevealPage>
+                    <p tw="mt-8 text-sm text-gray-600 text-center">
+                      Already have an account?{" "}
+                      <Link to={signInUrl}>
+                        <span tw="border-b border-gray-500 border-dotted">
+                          Sign In
+                        </span>
+                      </Link>
+                    </p>
+                  </Form>
+                </FormContainer>
+              </MainContent>
+            </MainContainer>
+            <IllustrationContainer>
+              <IllustrationImage imageSrc={illustrationImageSrc} />
+            </IllustrationContainer>
+          </Content>
+        </Container>
+      </AnimationRevealPage>
+    </>
   );
 };
